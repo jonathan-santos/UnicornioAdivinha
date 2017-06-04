dcac24cd571a45f79090c6cc9e5b37b000b48cc3
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2017-06-04 18:41:31 -0300

    emotion api implementada no controller2

--- js/controllers/controller2.js
+++ js/controllers/controller2.js
@@ -2,4 +2,26 @@ angular.module('unicornio').controller('Template2Controller', Template2Controlle
 
 function Template2Controller($scope,$firebaseArray,$state) {
 
+    $scope.url = "http://farm1.static.flickr.com/121/315138140_234d62d9aa.jpg";
+
+    $scope.enviar = function(){
+        $.ajax({
+            url: "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize",
+            beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","cefd8a4df9284c0eb966f96999b1d67a");
+            },
+            type: "POST",
+            data: "{ url: '" + $scope.url + "' }",
+        })
+        .done(function(data) {
+            console.log("success");
+            console.log(data);
+        })
+        .fail(function() {
+            console.log("error");
+        });
+    };
+
 }
