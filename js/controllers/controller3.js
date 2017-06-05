96b479e7b327db91cb1762235641061168b4ff83
Manoela Silverio Gomes <Manoela@Air-de-Manoela.home>
2017-06-04 22:08:04 -0300

    Resultado ajustado

--- js/controllers/controller3.js
+++ js/controllers/controller3.js
@@ -1,9 +1,59 @@
 angular.module('unicornio').controller('Template3Controller', Template3Controller)
 
 function Template3Controller($scope,$firebaseArray,$state,$http,emotionApiService) {
-    $scope.quote = null;
+    var resultado = emotionApiService.GetResultadoApi();
 
-    console.log(emotionApiService.GetResultadoApi());
+    $scope.sentimentos = [
+        {
+            sentimento: 'raiva',
+            valor: resultado.scores.anger
+        },
+        {
+            sentimento: 'desprezo',
+            valor: resultado.scores.contempt
+        },
+        {
+            sentimento: 'nojo',
+            valor: resultado.scores.disgust
+        },
+        {
+            sentimento: 'medo',
+            valor: resultado.scores.fear
+        },
+        {
+            sentimento: 'felicidade',
+            valor: resultado.scores.happiness
+        },
+        {
+            sentimento: 'neutro',
+            valor: resultado.scores.neutral
+        },
+        {
+            sentimento: 'tristeza',
+            valor: resultado.scores.sadness
+        },
+        {
+            sentimento: 'surpresa',
+            valor: resultado.scores.surprise
+        }
+    ];
+
+    $scope.maiorEmocao = 1;
+    $scope.quote = null;
+    var a = 0;
+
+    function encontrarSentimento() {
+        if ($scope.maiorEmocao >= $scope.sentimentos[a].valor) {
+            $scope.maiorEmocao = $scope.sentimentos[a].sentimento;
+            console.log($scope.maiorEmocao);
+            a+=1;
+            if (a==8) {
+                return
+            }
+        }
+    }
+
+    encontrarSentimento();
 
     var parametros = {
         Method: 'GET',
@@ -17,4 +67,8 @@ function Template3Controller($scope,$firebaseArray,$state,$http,emotionApiServic
         $scope.erro = true;
         $scope.quote = '';
     });
+
+    $scope.reiniciar = function() {
+        $state.go('apresentacao');
+    }
 }
